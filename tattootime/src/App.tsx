284e58c1613c42768c8de216a420af37f45107fa
tibo47-161 <tobi196183@gmail.com>
2025-06-18 16:09:25 +0200

    fix: Feature-Routen mit Dummy-Daten für Review, Payment, Materialverwaltung und Kundenhistorie eingebunden

--- tattootime/src/App.tsx
+++ tattootime/src/App.tsx
@@ -7,6 +7,11 @@ import Register from './pages/Register';
 import { ThemeProvider, createTheme } from '@mui/material';
 import { blue, grey } from '@mui/material/colors';
 import './App.css';
+import ReviewForm from './components/Review/ReviewForm';
+import MaterialManagement from './components/Admin/MaterialManagement';
+import PaymentForm from './components/Payment/PaymentForm';
+import CustomerHistory from './components/Customer/CustomerHistory';
+import { Appointment } from './types';
 
 const theme = createTheme({
   palette: {
@@ -53,6 +58,64 @@ const App: React.FC = () => {
               }
             />
             <Route path="/" element={<Navigate to="/login" />} />
+            <Route
+              path="/review"
+              element={
+                <PrivateRoute>
+                  <ReviewForm
+                    open={true}
+                    onClose={() => { /* Dummy */ }}
+                    appointment={{
+                      id: 'dummy',
+                      date: '2025-06-18',
+                      time: '17:00',
+                      clientName: 'Max Mustermann',
+                      service: 'Test-Tattoo',
+                      userId: 'dummy-user',
+                      serviceType: 'Tattoo',
+                    }}
+                    onReviewSuccess={() => { /* Dummy */ }}
+                  />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/material-management"
+              element={
+                <PrivateRoute>
+                  <MaterialManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/payment"
+              element={
+                <PrivateRoute>
+                  <PaymentForm
+                    open={true}
+                    onClose={() => { /* Dummy */ }}
+                    appointment={{
+                      id: 'dummy',
+                      date: '2025-06-18',
+                      time: '17:00',
+                      clientName: 'Max Mustermann',
+                      service: 'Test-Tattoo',
+                      userId: 'dummy-user',
+                      serviceType: 'Tattoo',
+                    }}
+                    onPaymentSuccess={() => { /* Dummy */ }}
+                  />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/customer-history"
+              element={
+                <PrivateRoute>
+                  <CustomerHistory />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </Router>
       </AuthProvider>
